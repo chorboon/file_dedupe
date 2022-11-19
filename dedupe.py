75593a73db19b6847dd3b8087e62c94b975fa614
James Tan <tan.james@infogrid.io>
2022-11-19 11:36:44 +0800

    Hash only 1GB due to memory limit on large files

--- dedupe.py
+++ dedupe.py
@@ -4,9 +4,11 @@ import glob,hashlib,os,sqlite3
 
 DEFAULT_DIR="./"
 DIR=DEFAULT_DIR
+BUFFER_LIMIT = 1000000000
 
 database = sqlite3.connect("database.db")
 cursor = database.cursor()
+
 
 #create a table if doesnt already exit
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='hashdb'")
@@ -24,9 +26,13 @@ print(files)
 for filename in files:
     if os.path.isdir(filename):
         continue
+    filesize = os.path.getsize(filename)
+    if filesize == 0:
+        filename_hash = 0
+        continue
     myPict = open(filename, 'rb')
     hasher = hashlib.md5()
-    buf = myPict.read()
+    buf = myPict.read(BUFFER_LIMIT)
     hasher.update(buf)
 
     
